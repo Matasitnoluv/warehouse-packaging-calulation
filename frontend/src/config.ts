7a48852a3823659455d02da24ff8ba2406573a41
Matasitnoluv <matasitchamp@gmail.com>
2025-07-17 19:54:11 +0700

    upcloud

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // API configuration
-export const API_URL = 'http://localhost:8081';
+export const API_URL = 'http://119.59.103.163:8081';
 
 // Other global configuration settings can be added here
 export const APP_NAME = 'Warehouse Management System';
